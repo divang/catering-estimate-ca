c69333e495fabc923dc8dda1a5538124e4334b89
Divang Sharma <divang.s@gmail.com>
2025-09-12 09:22:40 +0000

    Generated by Spark: Fix all reported errors.

--- src/App.tsx
+++ src/App.tsx
@@ -608,6 +608,7 @@ function App() {
                   Register
                 </Button>
               </div>
+            )}
             <Button 
               variant="outline" 
               onClick={() => setShowAdminLogin(true)}
